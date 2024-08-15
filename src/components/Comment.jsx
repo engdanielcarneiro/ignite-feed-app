16fb793826f6de15ce07b808f90e1eab0a1e7e86
Carneiro <daniel.carneiro@accruent.com>
2024-08-15 19:12:12 -0300

    feat: implementing creating post logic and using states

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,7 @@ import { ThumbsUp, Trash } from '@phosphor-icons/react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
 
-export function Comment() {
+export function Comment({content}) {
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/engdanielcarneiro.png" alt="" />
@@ -21,7 +21,7 @@ export function Comment() {
 
                     </header>
 
-                    <p>Muito bom Devon, parabéns!</p>
+                    <p>{content}</p>
                 </div>
 
                 <footer>
